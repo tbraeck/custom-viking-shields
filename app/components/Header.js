47d7ea137010bc9363f8d6f779ff84bcf083b3e7
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2025-01-24 18:36:15 -0700

    new commit

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import Image from 'next/image';
 import { Link } from 'react-router-dom';
@@ -8,19 +8,26 @@ function Header() {
   return (
     <header className="header">
       <div className="header-background">
-      <div className="logo">
-          <Link to="/">
-            <Image
-              src="/assets/logo.png"
-              width={100}
-              height={100}
-              alt="Viking Shields & Weapons Logo"
-              className="logo-image"
-            />
-          </Link>
-        </div>
         <div className="header-content">
-          <h1 className="title">Viking Shields & Weapons</h1>
+          <div className="logo">
+            <Link to="/">
+              <Image
+                src="/viking_shield.png"
+                width={100}
+                height={100}
+                alt="Viking Shields & Weapons Logo"
+                className="logo-image"
+              />
+            </Link>
+          </div>
+          
+          {/* Title Section */}
+          <div className="title-container">
+            <h1 className="title">Custom Wood</h1>
+            <h1 className="title">Shields & Weapons</h1>
+          </div>
+
+          {/* Navigation Menu */}
           <nav className="menu">
             <Link to="/about">About</Link>
             <Link to="/gallery">Gallery</Link>
