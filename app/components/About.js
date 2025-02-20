7454c51b23f14cad86b506e939a3d93ecf00e278
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2025-02-20 10:55:26 -0700

    messing with footer and about

--- app/components/About.js
+++ app/components/About.js
@@ -2,7 +2,7 @@
 function About() {
   return (
     <div className="about">
-      <h1>About Us</h1>
+      <h1>ABOUT US</h1>
       <p>We specialize in creating custom, handcrafted Viking shields and weapons for children. Perfect for play, costumes, and imaginative adventures!</p>
     </div>
   );
